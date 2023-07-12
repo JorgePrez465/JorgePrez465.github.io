58a1bed92ff0b02844ce81b987d6c93c915ab418
Jorge Perez <jorgeperez@macbook.mynetworksettings.com>
2023-07-12 17:30:18 -0400

    Added list in modal and index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function group1() {
+  // code 
+  console.log("entered group 1 ")
+}
+
+function group2() {
+  // code 
+  console.log("entered group 2 ")
+}
+
+function group3() {
+  // code 
+  console.log("entered group 3 ")
+}
+
+function group4() {
+  // code 
+  console.log("entered group 4 ")
+}
