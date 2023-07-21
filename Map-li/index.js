1c1303cbdb9bf5e3366e1c707bb0236be3245d23
Jorge Perez <jorgeperez@Jorges-MacBook-Pro.local>
2023-07-21 11:06:06 -0400

    Change to fadu project

---
+++ Map-li/index.js
@@ -0,0 +1,22 @@
+// function displayImage() {
+//   var id = document.getElementById("idInput").value;
+
+//   var imageMapping = {
+//     image1: "T1R1P9/LD.png",
+//     image2: "T1R1P9/LH.png",
+//     image3: "",
+//     image4: "T1R1P9/UH.png",
+//   };
+
+//   var imgElement = document.getElementById(id);
+//   if (imgElement) {
+//     var imageName = imageMapping[id]; // Step 2: Match the ID with an image name from the data structure
+//     if (imageName) {
+//       imgElement.src = imageName; // Step 4: Display the image by setting the src attribute
+//     } else {
+//       console.log("No image found for the provided ID.");
+//     }
+//   } else {
+//     console.log("Invalid ID.");
+//   }
+// }
